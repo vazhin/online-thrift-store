b5c6af9851ba9194af9f3ac1bc56988f5dc92495
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-10-27 03:16:22 +0300

    adds an ejs form for login & signup

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', (req, res) => {
+  res.render('login-signup-form', { form: 'Login' });
+});
+
+router.get('/signup', (req, res) => {
+  res.render('login-signup-form', { form: 'Sign Up' });
+});
+
 module.exports = router;
