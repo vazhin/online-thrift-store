2b2acadde6d46ab32b5dc183762842d83eb77b94
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-11-08 01:32:10 +0300

    renders the pagination bar currectly

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,1 @@
-const pageNum = getParam('page');
 
-function getParam(paramName) {
-  return decodeURIComponent(
-    location.search
-      .substr(1)
-      .split('&')
-      .reduce(
-        (a, elem) =>
-          elem.split('=')[0] === paramName ? a + elem.split('=')[1] : a,
-        ''
-      )
-  );
-}
