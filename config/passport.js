19b0a9b6869c5e78a21333f68ff00a4ef7bc2a7b
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-11-10 03:58:51 +0300

    logs user in

--- config/passport.js
+++ config/passport.js
@@ -2,59 +2,34 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 
-const User = require('../data/User');
-const Database = require('../data/Database');
+const { User } = require('../models');
 
 passport.use(
-  new LocalStrategy(function (username, password, done) {
-    const db = Database.open();
+  new LocalStrategy(async function (username, password, done) {
+    const user = await User.findOne({ where: { username: username } });
+    if (!user) return done(null, false, { message: 'Incorrect username.' });
 
-    db.get(`SELECT * FROM users WHERE username = ?`, [username], function (
-      err,
-      row
-    ) {
-      if (err) {
-        Database.close(db);
-        return done(err);
+    bcrypt.compare(password, user.password, (err, doesMatch) => {
+      if (err) return done(err);
+      if (!doesMatch) {
+        return done(null, false, { message: 'Incorrect password.' });
       }
-
-      if (!row) {
-        Database.close(db);
-        return done(null, false, { message: 'Incorrect email.' });
-      }
-
-      bcrypt.compare(password, row.password, (err, doesMatch) => {
-        if (err) {
-          Database.close(db);
-          return done(err);
-        }
-        if (!doesMatch) {
-          Database.close(db);
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-
-        const user = {
-          id: row.user_id,
-          username: row.username,
-          email: row.email,
-          hash: row.password,
-        };
-
-        Database.close(db);
-        return done(null, user);
-      });
+      return done(null, user);
     });
   })
 );
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  done(null, user.userId); // only the user ID is serialized to the session
 });
 
 passport.deserializeUser(async (userId, done) => {
   try {
-    const user = await User.get(userId);
-    done(null, user);
+    const user = await User.findOne({
+      where: { userId: userId },
+      attributes: ['username', 'email', 'image', 'userId'],
+    });
+    done(null, user.toJSON()); // this will be restored to req.user
   } catch (err) {
     done(err);
   }
